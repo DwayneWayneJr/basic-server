30a13e5c15cf8c5fb7be47e7b248b558a08de63b
DwayneWayneJr <dwaynejgrant@gmail.com>
2019-10-18 16:56:11 -0700

    corrected public folder

--- server.js
+++ server.js
@@ -8,8 +8,6 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.static('./public'));
 
-// app.get('/', (request, response) => response.send('Hello World!'));
-
 app.get('/hello', (request, response) => response.send('Hi Roz, thanks for the cookies. Hi James - Happy Birthday'));
 
 app.listen(PORT, () => console.log(`app listening on port ${PORT}!`));
