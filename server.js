8944a3a9aa5bd3de4c2df8117cc383a541fc0a4c
DwayneWayneJr <dwaynejgrant@gmail.com>
2019-10-18 17:12:33 -0700

    trying to get app to work

--- server.js
+++ server.js
@@ -8,6 +8,4 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.static('./public'));
 
-app.get('/hello', (request, response) => response.send('Hi Roz, thanks for the cookies. Hi James - Happy Birthday'));
-
 app.listen(PORT, () => console.log(`app listening on port ${PORT}!`));
